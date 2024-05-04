3c9dd3e40730c93c0dbc7d367bf3f211da06b18b
Tentastisch <studiotent22@gmail.com>
2024-05-04 11:51:49 +0200

    added: struct to stop running background processes

---
+++ src-tauri/src/app/app_state.rs
@@ -0,0 +1,23 @@
+use tauri::{State, Window};
+use std::sync::{Arc, Mutex};
+use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub stop_download: Arc<Mutex<bool>>,
+    pub stop_game: Arc<Mutex<bool>>,
+}
+
+impl AppState {
+    pub fn new() -> (Self, mpsc::Receiver<String>) {
+        let (tx, rx) = mpsc::channel(32);
+        (
+            AppState {
+                stop_download: Arc::new(Mutex::new(false)),
+                stop_game: Arc::new(Mutex::new(true)),
+            },
+            rx,
+        )
+    }
+}
