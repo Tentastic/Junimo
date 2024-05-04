7f9e3eb8ddf2746db34fdaabe55c63f8f727e175
Tentastisch <studiotent22@gmail.com>
2024-05-04 12:05:32 +0200

    added: default loading of theme and Config path

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,26 @@ import Home from "./pages/home";
 import Test from "./pages/test";
 import "./styles.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Config from "./pages/config.tsx";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === null) {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.body.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        }
+        else {
+            localStorage.setItem('theme', 'light');
+        }
+        return;
+    }
+
+    if (currentTheme !== 'light') {
+        document.body.classList.add(currentTheme);
+    }
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -11,6 +31,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
           <Routes>
               <Route index element={<Home />} />
               <Route path="test" element={<Test />} />
+              <Route path="config" element={<Config />} />
           </Routes>
       </BrowserRouter>
   </React.StrictMode>,
