ba1c67bf171c7ee9a3a1cd1d503b5dfef7cff241
Tentastisch <studiotent22@gmail.com>
2024-05-08 19:30:04 +0200

    moved: Wrap from home to downloader

--- src/components/Downloader.tsx
+++ src/components/Downloader.tsx
@@ -12,8 +12,9 @@ import {
     ContextMenuItem,
     ContextMenuTrigger,
 } from "@components/ui/context-menu"
+import {TabsContent} from "@components/ui/tabs.tsx";
 
-export default function Downloader({downloadList, setDownloadList, className}: {downloadList: Download[], setDownloadList: Dispatch<SetStateAction<Download[]>>, className: string}) {
+export default function Downloader({downloadList, className}: {downloadList: Download[], className: string}) {
     async function stopDownload() {
         await invoke('stop_download');
     }
@@ -29,50 +30,38 @@ export default function Downloader({downloadList, setDownloadList, className}: {
         }
     }
 
-    useEffect(() => {
-        const handleNewData = (event: any) => {
-            const data = event.payload as Download;
-            setDownloadList(prevLines => {
-                const newDownloads = [...prevLines];
-
-                if (prevLines.find(x => x.name === data.name) === undefined) {
-                    newDownloads.push(data);
-                    return newDownloads;
-                }
-
-                return prevLines.map(x => x.name === data.name ? data : x);
-            });
-        };
-
-        let unsubscribeEvent = listen('download', handleNewData);
-
-        return () => {
-            unsubscribeEvent.then((unsub) => unsub());
-        };
-    }, []);
-
     return (
-        <div className={clsx(
-            "flex flex-col gap-2 w-full h-full mt-6 overflow-y-auto pr-4",
-            className
-        )}>
-            {downloadList.map((mod, index) => (
-                <ContextMenu>
-                    <ContextMenuTrigger>
-                        <div key={index} className="w-full transform duration-150 cursor-pointer bg-muted hover:bg-muted-dark rounded-lg pb-0 overflow-hidden">
-                            <div className="w-full flex justify-between p-2 pb-1">
-                                <p>{mod.name}</p>
-                                <p className="text-zinc-500 h-2">{bytesToString(mod.current)} / {bytesToString(mod.size)}</p>
-                            </div>
-                            <Progress value={mod.current / mod.size * 100}/>
-                        </div>
-                    </ContextMenuTrigger>
-                    <ContextMenuContent>
-                        <ContextMenuItem onClick={stopDownload}>Cancel</ContextMenuItem>
-                    </ContextMenuContent>
-                </ContextMenu>
-                )
-            )}
-        </div>
+        <TabsContent value="download" className="flex-1 mt-7">
+            <div className="relative flex-grow h-full p-4 pr-0 pt-0 border-border border rounded-lg">
+                <div className="absolute -top-5 left-2 bg-background p-2 px-4">
+                    <h2 className="text-lg">Downloads</h2>
+                </div>
+                <div className={clsx(
+                    "flex flex-col gap-2 w-full h-full mt-6 overflow-y-auto pr-4",
+                    className
+                )}>
+                    {downloadList.map((mod, index) => (
+                            <ContextMenu>
+                                <ContextMenuTrigger>
+                                    <div key={index}
+                                         className="w-full transform duration-150 cursor-pointer bg-muted hover:bg-muted-dark rounded-lg pb-0 overflow-hidden">
+                                        <div className="w-full flex justify-between p-2 pb-1">
+                                            <p>{mod.name}</p>
+                                            <p className="text-zinc-500 h-2">{bytesToString(mod.current)} / {bytesToString(mod.size)}</p>
+                                        </div>
+                                        <Progress value={mod.current / mod.size * 100}/>
+                                    </div>
+                                </ContextMenuTrigger>
+                                <ContextMenuContent>
+                                    {!mod.aborted && (
+                                        <ContextMenuItem onClick={stopDownload}>Cancel</ContextMenuItem>
+                                    )}
+                                </ContextMenuContent>
+                            </ContextMenu>
+                        )
+                    )}
+                </div>
+            </div>
+        </TabsContent>
     )
 }
