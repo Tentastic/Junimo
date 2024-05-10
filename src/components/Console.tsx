1e015fc5fd6318346287c3f3be25ce6c2d654022
Tentastisch <studiotent22@gmail.com>
2024-05-10 20:39:03 +0200

    added: Console is now resizable

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -1,6 +1,6 @@
 import {listen} from "@tauri-apps/api/event";
 import {Download} from "@models/download.ts";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {clsx} from "clsx";
 import {ConsoleModel} from "@models/console.ts";
 import {invoke} from "@tauri-apps/api/core";
@@ -8,6 +8,56 @@ import {ModInfos} from "@models/mods.ts";
 
 export default function Console({playing, bigConsole}: {playing: boolean, bigConsole: boolean}) {
     const [lines, setLines] = useState<string[]>([]);
+
+    const [size, setSize] = useState({ width: 200, height: 128 });
+    const [isResizing, setIsResizing] = useState(false);
+    const [startPos, setStartPos] = useState({ x: 0, y: 0 });
+    const [flexReverse, setFlexReverse] = useState(false);
+    const containerRef = useRef(null);
+
+    function checkReverse(newHeight: number) {
+        const containerCheck = containerRef.current;
+        if (!containerCheck) return;
+        const container = containerCheck as HTMLElement;
+        const isOverflowing = container.scrollHeight > newHeight;
+        setFlexReverse(isOverflowing);
+    }
+
+    const onMouseDown = (e: React.MouseEvent) => {
+        setIsResizing(true);
+        setStartPos({
+            x: e.clientX,
+            y: e.clientY,
+        });
+
+        const html = document.documentElement;
+        html.classList.add("select-none");
+    };
+
+    const onMouseMove = (e: MouseEvent) => {
+        if (isResizing) {
+            const newWidth = size.width + e.clientX - startPos.x;
+            let newHeight = size.height + startPos.y - e.clientY;
+
+            const sev = document.documentElement.clientHeight * 0.5;
+
+            if (newHeight < 128)
+                newHeight = 128;
+            else if (newHeight > sev)
+                newHeight = sev;
+
+            setSize({ width: newWidth, height: newHeight });
+            setStartPos({ x: e.clientX, y: e.clientY });
+
+            checkReverse(newHeight);
+        }
+    };
+
+    const onMouseUp = () => {
+        setIsResizing(false);
+        const html = document.documentElement;
+        html.classList.remove("select-none");
+    };
 
 
     function addLine(line: ConsoleModel) {
@@ -42,6 +92,8 @@ export default function Console({playing, bigConsole}: {playing: boolean, bigCon
         setLines(data);
     }
 
+
+
     useEffect(() => {
         const handleNewData = (event: any) => {
             const data = event.payload as ConsoleModel;
@@ -54,6 +106,7 @@ export default function Console({playing, bigConsole}: {playing: boolean, bigCon
             else if (data.mode === 2) {
                 addLine(data);
             }
+            checkReverse(size.height);
         };
 
         let unsubscribeEvent = listen('console', handleNewData);
@@ -64,15 +117,39 @@ export default function Console({playing, bigConsole}: {playing: boolean, bigCon
         };
     }, []);
 
+    useEffect(() => {
+        if (isResizing) {
+            window.addEventListener('mousemove', onMouseMove);
+            window.addEventListener('mouseup', onMouseUp);
+        } else {
+            window.removeEventListener('mousemove', onMouseMove);
+            window.removeEventListener('mouseup', onMouseUp);
+        }
+
+        // Cleanup function to remove event listeners
+        return () => {
+            window.removeEventListener('mousemove', onMouseMove);
+            window.removeEventListener('mouseup', onMouseUp);
+        };
+    }, [isResizing]);
+
     return (
-        <div className={clsx(
-            "w-full h-full flex overflow-auto",
-            (!playing && !bigConsole) && lines.length > 5 || (playing || bigConsole) && lines.length > 21 ? "flex-col-reverse" : "flex-col mt-1"
-        )}>
-            <div>
-                {lines.map((line, index) => (
-                    <div key={index} dangerouslySetInnerHTML={{__html: line}}></div>
-                ))}
+        <div style={{ height: `${size.height}px` }}
+            className={clsx(
+            "h-32 min-h-32 max-h-[50vh] border-border bg-card hover:bg-muted transition duration-150 cursor-pointer pl-2 col-span-4 pb-2 border rounded-lg w-full overflow-hidden",
+        )}
+            >
+            <div className="w-full h-2 min-h-2 max-h-2 cursor-n-resize"
+                 onMouseDown={onMouseDown} />
+            <div ref={containerRef} className={clsx(
+                "h-full flex overflow-auto",
+                flexReverse ? "flex-col-reverse" : "flex-col",
+            )}>
+                <div>
+                    {lines.map((line, index) => (
+                        <div key={index} dangerouslySetInnerHTML={{__html: line}}></div>
+                    ))}
+                </div>
             </div>
         </div>
     )
