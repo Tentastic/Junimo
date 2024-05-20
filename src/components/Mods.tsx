d5d2bf37dff28afd9c4b5992aacae6813313bb50
Tentastisch <studiotent22@gmail.com>
2024-05-20 17:51:34 +0200

    added + moved: Added Groups and moved props to separate mods provider

--- src/components/Mods.tsx
+++ src/components/Mods.tsx
@@ -1,6 +1,6 @@
 import {invoke} from "@tauri-apps/api/core";
 import {Profile} from "@models/profile.ts";
-import {Dispatch, SetStateAction, useEffect} from "react";
+import {Dispatch, SetStateAction, useEffect, useState} from "react";
 import Wrapper from "@components/ui/wrapper.tsx";
 import {clsx} from "clsx";
 import {Input} from "@components/ui/input.tsx";
@@ -8,89 +8,152 @@ import {ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger} fr
 import UninstallMod from "@components/UninstallMod.tsx";
 import UninstallAll from "@components/UninstallAll.tsx";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger, } from "@components/ui/dropdown-menu"
+import {useModsState} from "@components/ModsProvider.tsx";
+import {ModInfos} from "@models/mods.ts";
+import {useTranslation} from "react-i18next";
 
-export default function Mods({setKey, profile, setProfile, selected, setSelected}:
-    {setKey: Dispatch<SetStateAction<number>>, profile: Profile | undefined, setProfile: Dispatch<SetStateAction<Profile | undefined>>, selected: number[], setSelected: Dispatch<SetStateAction<number[]>>}) {
+export default function Mods() {
+    const { profile, selectedRemove } = useModsState();
+    const [groupedMods, setGroupedMods] = useState<{ [key: string]: ModInfos[] }>({});
+    const { t } = useTranslation('home');
+
+    const groupModsByGroup = (mods: ModInfos[] | undefined): { [p: string]: ModInfos[] } => {
+        if (mods === undefined) return {};
+        return mods.reduce((groups, mod) => {
+            const group = mod.group || 'Ungrouped';
+            if (!groups[group]) {
+                groups[group] = [];
+            }
+            groups[group].push(mod);
+            return groups;
+        }, {} as { [key: string]: ModInfos[] });
+    };
 
     async function loadProfile() {
         const profilePath = await invoke<string>('profile_path');
-        const newProfile = await invoke<Profile>('get_current_profile', {path: profilePath});
-        setProfile(newProfile);
+        const loadedMods = await invoke<Profile>('get_current_profile', {path: profilePath});
+
+        console.log(loadedMods);
+        profile[1](loadedMods);
+        setGroupedMods(groupModsByGroup(loadedMods.mods));
+        //profile[1](newProfile);
     }
 
-    function setIndex(index: number) {
-        if (selected.includes(index)) {
-            setSelected(selected.filter(i => i !== index));
+    function setIndex(modNames: string) {
+        if (selectedRemove[0].includes(modNames)) {
+            selectedRemove[1](selectedRemove[0].filter(i => i !== modNames));
         }
         else {
-            setSelected([...selected, index]);
+            selectedRemove[1]([...selectedRemove[0], modNames]);
         }
     }
 
     function doSearch(search: string) {
-        if (profile?.mods !== undefined) {
-            for (let mod of profile?.mods) {
-                mod.invisible = !mod.name.toLowerCase().includes(search.toLowerCase());
-            }
-
-            setProfile({...profile});
+        if (profile[0] === undefined) return;
+        let searchedMods = profile[0].mods;
+        for (let mod of searchedMods) {
+            mod.invisible = !mod.name.toLowerCase().includes(search.toLowerCase());
         }
+        let newProfile = profile[0];
+        newProfile.mods = searchedMods;
+        profile[1](newProfile);
+        setGroupedMods(groupModsByGroup(searchedMods));
     }
 
     function unselectAll() {
-        setSelected([]);
+        selectedRemove[1]([]);
+    }
+
+    function toggleGroup(groupName: string) {
+        if (groupedMods[groupName].some(mod => selectedRemove[0].includes(mod.name))) {
+            selectedRemove[1](selectedRemove[0].filter(i => !groupedMods[groupName].some(mod => mod.name === i)));
+        } else {
+            selectedRemove[1]([...selectedRemove[0], ...groupedMods[groupName].map(mod => mod.name)]);
+        }
     }
 
     useEffect(() => {
         loadProfile();
-        setSelected([]);
+        selectedRemove[1]([]);
     }, []);
 
     return (
         <div className="relative w-full flex flex-col border-border pl-3 border rounded-lg flex-1">
             <div className="absolute -top-5 bg-background left-2 p-2 px-4">
-                <h2 className="text-lg">Current Mods</h2>
+                <h2 className="text-lg">{t("currentMods")}</h2>
             </div>
             <div className="flex gap-2 mt-6 mx-2 mr-6">
                 <div
                     className="w-full transform duration-150 cursor-pointer bg-muted hover:bg-muted-dark rounded-lg flex justify-between">
-                    <Input placeholder="Search..." onChange={x => doSearch(x.target.value)}/>
+                    <Input placeholder={t("search")} onChange={x => doSearch(x.target.value)}/>
                 </div>
-                <DropdownMenu>
-                    <DropdownMenuTrigger>
-                        <button
-                            className="w-12 h-full rounded transition duration-150 bg-muted hover:bg-muted-dark flex items-center justify-center">
-                            Sort
-                        </button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator/>
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
             </div>
             <div className="flex flex-col gap-2 w-full mt-2 overflow-auto p-1 pb-4 pr-4">
-                {profile?.mods.map((mod, index) => (
-                    <div key={index} className={clsx(
-                        mod.invisible && "hidden"
-                    )}>
-                        <ContextMenu>
-                            <ContextMenuTrigger>
-                                <div onClick={i => setIndex(index)}>
-                                    <Wrapper mod={mod} selected={selected.includes(index)}/>
-                                </div>
-                            </ContextMenuTrigger>
-                            <ContextMenuContent>
-                                <ContextMenuItem onClick={unselectAll}>Unselect all</ContextMenuItem>
-                                <UninstallMod name={mod.name} setKey={setKey}/>
-                                <UninstallAll mods={profile?.mods} numbers={selected} setKey={setKey}/>
-                            </ContextMenuContent>
-                        </ContextMenu>
-                    </div>
+                {Object.keys(groupedMods).map((group) => (
+                    <>
+                        {group === 'Ungrouped' ? (
+                            <>
+                                {groupedMods[group].map((mod, index) => (
+                                    <div key={index} className={clsx(
+                                        mod.invisible && "hidden"
+                                    )}>
+                                        <ContextMenu>
+                                            <ContextMenuTrigger>
+                                                <div onClick={i => setIndex(mod.name)}>
+                                                    <Wrapper mod={mod} selected={selectedRemove[0].includes(mod.name)}/>
+                                                </div>
+                                            </ContextMenuTrigger>
+                                            <ContextMenuContent>
+                                                <ContextMenuItem onClick={unselectAll}>{t("unselect")}</ContextMenuItem>
+                                                <UninstallMod name={mod.name} />
+                                                <UninstallAll mods={profile[0]?.mods} names={selectedRemove[0]} />
+                                            </ContextMenuContent>
+                                        </ContextMenu>
+                                    </div>
+                                ))}
+                            </>
+                        ) : (
+                            <ContextMenu>
+                                <ContextMenuTrigger>
+                                    <div key={group}
+                                         className={clsx(
+                                             "bg-groups transition duration-150 cursor-pointer hover:bg-groups-dark rounded-lg p-2",
+                                             groupedMods[group].filter(mod => mod.invisible).length === groupedMods[group].length && "hidden"
+                                         )}
+                                         onClick={x => toggleGroup(group)}>
+                                        <h2 className="text-lg text-muted-foreground mb-2">{group}</h2>
+                                        <div className="flex flex-col gap-2">
+                                            {groupedMods[group].map((mod, index) => (
+                                                <div key={index} className={clsx(
+                                                    mod.invisible && "hidden"
+                                                )}>
+                                                    <ContextMenu>
+                                                        <ContextMenuTrigger>
+                                                            <div>
+                                                                <Wrapper mod={mod}
+                                                                         selected={selectedRemove[0].includes(mod.name)}/>
+                                                            </div>
+                                                        </ContextMenuTrigger>
+                                                        <ContextMenuContent>
+                                                            <ContextMenuItem onClick={unselectAll}>{t('unselect')}</ContextMenuItem>
+                                                            <UninstallMod name={mod.name}/>
+                                                            <UninstallAll mods={profile[0]?.mods}
+                                                                          names={selectedRemove[0]}/>
+                                                        </ContextMenuContent>
+                                                    </ContextMenu>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </ContextMenuTrigger>
+                                <ContextMenuContent>
+                                    <ContextMenuItem onClick={unselectAll}>{t('unselect')}</ContextMenuItem>
+                                    <UninstallAll mods={profile[0]?.mods}
+                                                  names={selectedRemove[0]}/>
+                                </ContextMenuContent>
+                            </ContextMenu>
+                        )}
+                    </>
                 ))}
             </div>
         </div>
--- src/components/ModsInstalled.tsx
+++ src/components/ModsInstalled.tsx
@@ -1,63 +1,80 @@
 import {invoke} from "@tauri-apps/api/core";
 import {ModInfos} from "@models/mods.ts";
-import {Dispatch, SetStateAction, useEffect} from "react";
+import {useEffect, useState} from "react";
 import {clsx} from "clsx";
 import {ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger} from "@components/ui/context-menu.tsx";
 import {Input} from "@components/ui/input.tsx";
 import Wrapper from "@components/ui/wrapper.tsx";
-import {TabsContent} from "@components/ui/tabs.tsx";
 import UninstallMod from "@components/UninstallMod.tsx";
 import UninstallAll from "@components/UninstallAll.tsx";
-import {
-    DropdownMenu,
-    DropdownMenuContent, DropdownMenuItem,
-    DropdownMenuLabel, DropdownMenuSeparator,
-    DropdownMenuTrigger
-} from "@components/ui/dropdown-menu.tsx";
+import {useModsState} from "@components/ModsProvider.tsx";
+import {useTranslation} from "react-i18next";
 
 
-export default function ModsInstalled({setKey, modList, setModList, selected, setSelected}:
-    {setKey: Dispatch<SetStateAction<number>>, modList: ModInfos[], setModList: Dispatch<SetStateAction<ModInfos[]>>, selected: number[], setSelected: Dispatch<SetStateAction<number[]>>}) {
+export default function ModsInstalled() {
+    const { installedMods, selectedAdd } = useModsState();
+    const [groupedMods, setGroupedMods] = useState<{ [key: string]: ModInfos[] }>({});
+    const { t } = useTranslation('home');
+
+    const groupModsByGroup = (mods: ModInfos[]): { [key: string]: ModInfos[] } => {
+        return mods.reduce((groups, mod) => {
+            const group = mod.group || 'Ungrouped';
+            if (!groups[group]) {
+                groups[group] = [];
+            }
+            groups[group].push(mod);
+            return groups;
+        }, {} as { [key: string]: ModInfos[] });
+    };
 
     async function getMods() {
         const mods = await invoke<ModInfos[]>('get_installed_mods');
-        setModList(mods);
+        installedMods[1](mods);
+        setGroupedMods(groupModsByGroup(mods));
     }
 
     function doSearch(search: string) {
-        for (let mod of modList) {
+        for (let mod of installedMods[0]) {
             mod.invisible = !mod.name.toLowerCase().includes(search.toLowerCase());
         }
-
-        setModList([...modList]);
+        installedMods[1]([...installedMods[0]]);
+        setGroupedMods(groupModsByGroup(installedMods[0]));
     }
 
     function unselectAll() {
-        setSelected([]);
+        selectedAdd[1]([]);
     }
 
-    function setIndex(index: number) {
-        if (selected.includes(index)) {
-            setSelected(selected.filter(i => i !== index));
+    function setIndex(modName: string) {
+        if (selectedAdd[0].includes(modName)) {
+            selectedAdd[1](selectedAdd[0].filter(i => i !== modName));
         }
         else {
-            setSelected([...selected, index]);
+            selectedAdd[1]([...selectedAdd[0], modName]);
+        }
+    }
+
+    function toggleGroup(groupName: string) {
+        if (groupedMods[groupName].some(mod => selectedAdd[0].includes(mod.name))) {
+            selectedAdd[1](selectedAdd[0].filter(i => !groupedMods[groupName].some(mod => mod.name === i)));
+        } else {
+            selectedAdd[1]([...selectedAdd[0], ...groupedMods[groupName].map(mod => mod.name)]);
         }
     }
 
     useEffect(() => {
         getMods();
-        setSelected([]);
+        selectedAdd[1]([]);
 
         return () => {
-            setSelected([]);
+            selectedAdd[1]([]);
         }
     }, []);
 
     return (
         <div className="relative h-full flex flex-col w-[30vw] border-border pl-3 border rounded-lg">
             <div className="absolute -top-5 bg-background left-2 p-2 px-4">
-                <h2 className="text-lg">Installed Mods</h2>
+                <h2 className="text-lg">{t("installedMods")}</h2>
             </div>
             <div className="flex gap-2 mt-6 mx-2 mr-6">
                 <div
@@ -66,23 +83,68 @@ export default function ModsInstalled({setKey, modList, setModList, selected, se
                 </div>
             </div>
             <div className="flex flex-col gap-2 w-full aboslute mt-2 overflow-auto relative p-1 pb-4 pr-4">
-                {modList.map((mod, index) => (
-                    <div key={index} className={clsx(
-                        mod.invisible && "hidden"
-                    )}>
-                        <ContextMenu>
-                            <ContextMenuTrigger>
-                                <div onClick={i => setIndex(index)}>
-                                    <Wrapper mod={mod} selected={selected.includes(index)}/>
-                                </div>
-                            </ContextMenuTrigger>
-                            <ContextMenuContent>
-                                <ContextMenuItem onClick={unselectAll}>Unselect all</ContextMenuItem>
-                                <UninstallMod name={mod.name} setKey={setKey}/>
-                                <UninstallAll mods={modList} numbers={selected} setKey={setKey}/>
-                            </ContextMenuContent>
-                        </ContextMenu>
-                    </div>
+                {Object.keys(groupedMods).map((group) => (
+                    <>
+                        {group === 'Ungrouped' ? (
+                            <>
+                                {groupedMods[group].map((mod, index) => (
+                                    <div key={index} className={clsx(
+                                        mod.invisible && "hidden"
+                                    )}>
+                                        <ContextMenu>
+                                            <ContextMenuTrigger>
+                                                <div onClick={i => setIndex(mod.name)}>
+                                                    <Wrapper mod={mod} selected={selectedAdd[0].includes(mod.name)}/>
+                                                </div>
+                                            </ContextMenuTrigger>
+                                            <ContextMenuContent>
+                                                <ContextMenuItem onClick={unselectAll}>{t("unselect")}</ContextMenuItem>
+                                                <UninstallMod name={mod.name} />
+                                                <UninstallAll mods={installedMods[0]} names={selectedAdd[0]} />
+                                            </ContextMenuContent>
+                                        </ContextMenu>
+                                    </div>
+                                ))}
+                            </>
+                        ) : (
+                            <ContextMenu>
+                                <ContextMenuTrigger>
+                                    <div key={group}
+                                         className="bg-groups transition duration-150 cursor-pointer hover:bg-groups-dark rounded-lg p-2"
+                                         onClick={x => toggleGroup(group)}>
+                                        <h2 className="text-lg text-muted-foreground mb-2">{group}</h2>
+                                        <div className="flex flex-col gap-2">
+                                            {groupedMods[group].map((mod, index) => (
+                                                <div key={index} className={clsx(
+                                                    mod.invisible && "hidden"
+                                                )}>
+                                                    <ContextMenu>
+                                                        <ContextMenuTrigger>
+                                                            <div>
+                                                                <Wrapper mod={mod}
+                                                                         selected={selectedAdd[0].includes(mod.name)}/>
+                                                            </div>
+                                                        </ContextMenuTrigger>
+                                                        <ContextMenuContent>
+                                                            <ContextMenuItem onClick={unselectAll}>{t("unselect")}</ContextMenuItem>
+                                                            <UninstallMod name={mod.name}/>
+                                                            <UninstallAll mods={installedMods[0]}
+                                                                          names={selectedAdd[0]}/>
+                                                        </ContextMenuContent>
+                                                    </ContextMenu>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </ContextMenuTrigger>
+                                <ContextMenuContent>
+                                    <ContextMenuItem onClick={unselectAll}>{t("unselect")}</ContextMenuItem>
+                                    <UninstallAll mods={installedMods[0]}
+                                                  names={selectedAdd[0]}/>
+                                </ContextMenuContent>
+                            </ContextMenu>
+                        )}
+                    </>
                 ))}
             </div>
         </div>
