539699f6b0e7b259ec253e204b640d3f04432daa
Tentastisch <studiotent22@gmail.com>
2024-05-20 18:13:00 +0200

    refactor: Changed config into separate pages

--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -8,139 +8,37 @@ import {User} from "@models/user";
 import NexusMods from "@assets/NexusMods.png";
 import {Switch} from "@components/ui/switch.tsx";
 import { open } from '@tauri-apps/plugin-dialog';
+import GeneralConfig from "@components/config/GeneralConfig.tsx";
+import CheckConfig from "@components/config/CheckConfig.tsx";
+
+function PageLoad({page}: {page: number}) {
+    switch (page) {
+        case 0:
+            return (
+                <GeneralConfig />
+            );
+        case 1:
+            return (
+                <CheckConfig />
+            );
+        default:
+            return (
+                <GeneralConfig />
+            );
+    }
+}
 
 export default function Config() {
-    const [] = useState("");
-    const [gamePath, setGamePath] = useState("");
-    const [user, setUser] = useState<User>();
-    const [nmxSwitch, setNmxSwitch] = useState(false);
-
-    async function loadConfig() {
-        try {
-            const configPath = await invoke<string>('config_path');
-            const config = await invoke<ConfigModel>('get_config', {path: configPath});
-            setGamePath(config.game_path);
-            setNmxSwitch(config.handle_nxm);
-        } catch (error) {
-            console.error('Failed to fetch configuration path:', error);
-        }
-    }
-
-    async function loadUser() {
-        const user = await invoke<User | null>('load_user');
-        if (user !== null)
-            setUser(user);
-    }
-
-    async function fetchConfigPath() {
-        const path = await open({
-            multiple: false,
-            directory: true,
-        });
-        if (path !== null) {
-            setGamePath(path);
-        }
-
-        //const configPath = await invoke<string>('config_path');
-        //const path = await invoke<string>('select_game_dir', {path: configPath});
-        //setGamePath(path);
-    }
-
-    async function save() {
-        const configPath = await invoke<string>('config_path');
-        const config : ConfigModel = {
-            init_app: true,
-            game_path: gamePath,
-            handle_nxm: nmxSwitch
-        }
-
-        const path = await invoke<ConfigModel>('save_config_button', {config: config, path: configPath});
-        setGamePath(path.game_path);
-    }
-
-    async function loginUser() {
-        const user = await invoke<User | null>('connect_user');
-        if (user !== null)
-            setUser(user);
-    }
-
-    async function logoutUser() {
-        await invoke('disconnect_user');
-        setUser(undefined);
-    }
-
-    async function registerNxm() {
-        await invoke('register_nxm');
-    }
-
-    useEffect(() => {
-        if (nmxSwitch) {
-            registerNxm();
-        }
-    }, [nmxSwitch]);
-
-    useEffect(() => {
-        loadConfig();
-        loadUser();
-    }, []);
+    const [page, setPage] = useState(0);
 
     return (
-        <div className="w-full h-[100vh] p-6 flex flex-col justify-start items-start">
-            <h1 className="text-3xl text-left font-bold mb-4 text-primary">Settings</h1>
-            <div className="w-full flex flex-col gap-4">
-                <div className="flex flex-col">
-                    <Label htmlFor="path" className="ml-1 mb-2 text-xl text-muted-foreground">Game Path</Label>
-                    <div className="flex gap-2">
-                        <Input id="path" placeholder="Please enter your game path..."
-                               value={gamePath} onChange={x => setGamePath(x.target.value)} />
-                        <button onClick={fetchConfigPath} className="w-10 h-10 flex items-center justify-center transition duration-150 border rounded-lg
-                        bg-muted hover:bg-muted-dark">
-                            <Folder size={20}/>
-                        </button>
-                    </div>
-                </div>
-                <div className="w-full h-[2px] border-lg bg-muted" />
-                <div>
-                    {user === undefined || user === null ? (
-                            <div className="flex gap-1 items-center">
-                                Login via
-                                <button onClick={loginUser}
-                                        className="p-1 px-2 ml-2 flex gap-2 transition duration-150 bg-amber-600 hover:bg-amber-700 rounded font-bold">
-                                    <img src={NexusMods} alt="NexusMods Logo" className="h-6 w-6"/>
-                                    Nexus Mods
-                                </button>
-                            </div>
-                        ) :
-                        (
-                            <div className="flex flex-col gap-2">
-                                <h2 className="text-xl text-muted-foreground">NexusMods Settings</h2>
-                                <div className="flex gap-2 items-center justify-between rounded-lg p-2 bg-muted">
-                                    <div className="flex items-center gap-2 text-lg">
-                                        <img src={NexusMods} alt="NexusMods Logo" className="h-8 w-8"/>
-                                        <p>Logged in as: <span className="font-bold">{user.name}</span></p>
-                                    </div>
-                                    <button onClick={logoutUser}
-                                            className="p-1 px-5 flex gap-2 transition duration-150 bg-destructive hover:brightness-75 rounded">
-                                        Logout
-                                    </button>
-                                </div>
-                                <div className="flex gap-2 items-center mt-4">
-                                    <Switch checked={nmxSwitch} onCheckedChange={x => setNmxSwitch(!nmxSwitch)}/>
-                                    Let Junimo handle the
-                                    <div className="bg-[#d98f40] p-1 px-2 w-fit flex gap-1">
-                                        <img src={NexusMods} alt="Nexus Mods Mod Manager Icon" className="h-full w-6" />
-                                        MOD MANAGER DOWNLOAD
-                                    </div>
-                                    Button
-                                </div>
-                            </div>
-                        )}
-                </div>
+        <div className="w-full h-[100vh] p-6 pl-6 pt-4 flex flex-col justify-start items-start">
+            <div className="flex gap-2 mb-4">
+                <div className="p-1 px-3 rounded bg-muted cursor-pointer" onClick={x => setPage(0)}>General</div>
+                <div className="p-1 px-3 rounded bg-muted cursor-pointer" onClick={x => setPage(1)}>Checks</div>
+                <div className="p-1 px-3 rounded bg-muted cursor-pointer" onClick={x => setPage(2)}>NexusMods</div>
             </div>
-            <div className="h-full w-full flex justify-end items-end">
-                <button onClick={save}
-                        className="p-2 px-6 transition duration-150 bg-green-500 hover:bg-green-600 rounded-lg text-white">Save</button>
-            </div>
+            <PageLoad page={page}/>
         </div>
     )
 }
