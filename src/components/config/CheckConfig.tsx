6d9abcf459c083d7d55691078ca95e75288c7f5a
Tentastisch <studiotent22@gmail.com>
2024-05-22 15:22:16 +0200

    added: Translations

--- src/components/config/CheckConfig.tsx
+++ src/components/config/CheckConfig.tsx
@@ -3,17 +3,20 @@ import {Input} from "@components/ui/input.tsx";
 import {Folder} from "lucide-react";
 import NexusMods from "@assets/NexusMods.png";
 import {Switch} from "@components/ui/switch.tsx";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {invoke} from "@tauri-apps/api/core";
 import {Config as ConfigModel} from "@models/config.ts";
 import {Checkbox} from "@components/ui/checkbox.tsx";
 import {useModsState} from "@components/ModsProvider.tsx";
 import {clsx} from "clsx";
+import Save from "@components/ui/save.tsx";
+import {useTranslation} from "react-i18next";
 
 
 export default function CheckConfig() {
-    const { reloadKey } = useModsState();
+    const { t } = useTranslation("config");
 
+    const { reloadKey } = useModsState();
     const [savedConfig, setSavedConfig] = useState<ConfigModel | undefined>();
     const [gamePath, setGamePath] = useState("");
     const [activateRequirements, setActivateRequirements] = useState(true);
@@ -60,7 +63,9 @@ export default function CheckConfig() {
                 activate_requirements: activateRequirements,
                 block_on_missing_requirements: blockOnMissingRequirements,
                 activate_broken: activateBroken,
-                block_on_broken: blockOnBroken
+                block_on_broken: blockOnBroken,
+                lang: savedConfig.lang,
+                keep_open: savedConfig.keep_open
             }
 
             const path = await invoke<ConfigModel>('save_config_button', {config: config, path: configPath});
@@ -75,24 +80,23 @@ export default function CheckConfig() {
     return (
         <>
             <div className="absolute right-5">
-                <button onClick={save}
-                        className="p-2 px-6 transition duration-150 bg-green-500 hover:bg-green-600 rounded-lg text-white">Save
-                </button>
+                <div onClick={save}>
+                    <Save/>
+                </div>
             </div>
-            <h1 className="text-3xl text-left font-bold mb-4 text-primary">Check Settings</h1>
+            <h1 className="text-3xl text-left font-bold mb-4 text-primary">{t("checksTitle")}</h1>
             <div className="w-full flex flex-col gap-4">
                 <div className="flex flex-col">
-                    <Label htmlFor="path" className="mb-1 text-xl">Activate requirement checks</Label>
-                    <p className="text-sm text-muted-foreground mb-2">Junimo will check for missing mod requirements</p>
+                    <Label htmlFor="path" className="mb-1 text-xl">{t("activeRequirement")}</Label>
+                    <p className="text-sm text-muted-foreground mb-2">{t("activeRequirementDesc")}</p>
                     <div className="flex gap-2">
                         <Checkbox checked={activateRequirements}
                                   onCheckedChange={e => setActivateRequirements(!activateRequirements)}/>
                     </div>
                 </div>
                 <div className={clsx("flex flex-col ml-5", activateRequirements ? "" : "opacity-50")}>
-                    <Label htmlFor="path" className="mb-1 text-xl">Block on missing requirements</Label>
-                    <p className="text-sm text-muted-foreground mb-2">Block Junimo from starting the game if you miss
-                        requirements</p>
+                    <Label htmlFor="path" className="mb-1 text-xl">{t("blockRequirement")}</Label>
+                    <p className="text-sm text-muted-foreground mb-2">{t("blockRequirementDesc")}</p>
                     <div className="flex gap-2">
                         <Checkbox checked={blockOnMissingRequirements}
                                   onCheckedChange={e => setBlockOnMissingRequirements(!blockOnMissingRequirements)}/>
@@ -100,8 +104,8 @@ export default function CheckConfig() {
                 </div>
                 <div className="w-full h-[2px] border-lg bg-muted"/>
                 <div className="flex flex-col">
-                    <Label htmlFor="path" className="mb-1 text-xl">Activate broken checks</Label>
-                    <p className="text-sm text-muted-foreground mb-2">Junimo will check for any mods that are broken in your current version</p>
+                    <Label htmlFor="path" className="mb-1 text-xl">{t("activeBroken")}</Label>
+                    <p className="text-sm text-muted-foreground mb-2">{t("activeBrokenDesc")}</p>
                     <div className="flex gap-2">
                         <Checkbox checked={activateBroken}
                                   onCheckedChange={e => setActivateBroken(!activateBroken)}/>
@@ -110,8 +114,8 @@ export default function CheckConfig() {
                 <div className={
                     clsx("flex flex-col ml-5", activateBroken ? "" : "opacity-50")
                 }>
-                    <Label htmlFor="path" className="mb-1 text-xl">Block on broken mods</Label>
-                    <p className="text-sm text-muted-foreground mb-2">Block Junimo from starting the game if any mods are broken</p>
+                    <Label htmlFor="path" className="mb-1 text-xl">{t("blockBroken")}</Label>
+                    <p className="text-sm text-muted-foreground mb-2">{t("blockBrokenDesc")}</p>
                     <div className="flex gap-2">
                         <Checkbox checked={blockOnBroken} onCheckedChange={e => setBlockOnBroken(!blockOnBroken)}/>
                     </div>
--- src/components/config/GeneralConfig.tsx
+++ src/components/config/GeneralConfig.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import { Input } from "@components/ui/input"
 import { Label } from "@components/ui/label"
 import { Folder } from 'lucide-react';
@@ -8,29 +8,33 @@ import {User} from "@models/user";
 import NexusMods from "@assets/NexusMods.png";
 import {Switch} from "@components/ui/switch.tsx";
 import { open } from '@tauri-apps/plugin-dialog';
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@components/ui/select.tsx";
+import ThemeSwitch from "@components/ThemeSwitch.tsx";
+import {Checkbox} from "@components/ui/checkbox.tsx";
+import Save from "@components/ui/save.tsx";
+import {useTranslation} from "react-i18next";
 
 export default function GeneralConfig() {
     const [savedConfig, setSavedConfig] = useState<ConfigModel | undefined>();
     const [gamePath, setGamePath] = useState("");
-    const [user, setUser] = useState<User>();
-    const [nmxSwitch, setNmxSwitch] = useState(false);
+    const [lang, setLang] = useState("en");
+    const [keepOpen, setKeepOpen] = useState(true);
+
+    const { t, i18n } = useTranslation("config");
 
     async function loadConfig() {
         try {
             const configPath = await invoke<string>('config_path');
             const config = await invoke<ConfigModel>('get_config', {path: configPath});
             setGamePath(config.game_path);
-            setNmxSwitch(config.handle_nxm);
+            if (config.lang !== null)
+                setLang(config.lang);
+            if (config.keep_open !== null)
+                setKeepOpen(config.keep_open);
             setSavedConfig(config);
         } catch (error) {
             console.error('Failed to fetch configuration path:', error);
         }
-    }
-
-    async function loadUser() {
-        const user = await invoke<User | null>('load_user');
-        if (user !== null)
-            setUser(user);
     }
 
     async function fetchConfigPath() {
@@ -49,55 +53,36 @@ export default function GeneralConfig() {
             const config : ConfigModel = {
                 init_app: true,
                 game_path: gamePath,
-                handle_nxm: nmxSwitch,
+                handle_nxm: savedConfig.handle_nxm,
                 activate_requirements: savedConfig.activate_requirements,
                 block_on_missing_requirements: savedConfig.block_on_missing_requirements,
                 activate_broken: savedConfig.activate_broken,
-                block_on_broken: savedConfig.block_on_broken
+                block_on_broken: savedConfig.block_on_broken,
+                lang: lang,
+                keep_open: keepOpen
             }
 
             const path = await invoke<ConfigModel>('save_config_button', {config: config, path: configPath});
             setGamePath(path.game_path);
+            i18n.changeLanguage(lang).then(r => console.log(r)).catch(e => console.error(e));
         }
     }
-
-    async function loginUser() {
-        const user = await invoke<User | null>('connect_user');
-        if (user !== null)
-            setUser(user);
-    }
-
-    async function logoutUser() {
-        await invoke('disconnect_user');
-        setUser(undefined);
-    }
-
-    async function registerNxm() {
-        await invoke('register_nxm');
-    }
-
-    useEffect(() => {
-        if (nmxSwitch) {
-            registerNxm();
-        }
-    }, [nmxSwitch]);
 
     useEffect(() => {
         loadConfig();
-        loadUser();
     }, []);
 
     return (
         <>
             <div className="absolute right-5">
-                <button onClick={save}
-                        className="p-2 px-6 transition duration-150 bg-green-500 hover:bg-green-600 rounded-lg text-white">Save
-                </button>
+                <div onClick={save}>
+                    <Save />
+                </div>
             </div>
-            <h1 className="text-3xl text-left font-bold mb-4 text-primary">General Settings</h1>
+            <h1 className="text-3xl text-left font-bold mb-4 text-primary">{t("generalTitle")}</h1>
             <div className="w-full flex flex-col gap-4">
                 <div className="flex flex-col">
-                    <Label htmlFor="path" className="ml-1 mb-2 text-xl">Game Path</Label>
+                    <Label htmlFor="path" className="ml-1 mb-2 text-xl">{t("gamePath")}</Label>
                     <p className="text-sm text-muted-foreground"></p>
                     <div className="flex gap-2">
                         <Input id="path" placeholder="Please enter your game path..."
@@ -109,41 +94,29 @@ export default function GeneralConfig() {
                     </div>
                 </div>
                 <div className="w-full h-[2px] border-lg bg-muted"/>
-                <div>
-                    {user === undefined || user === null ? (
-                            <div className="flex gap-1 items-center">
-                                Login via
-                                <button onClick={loginUser}
-                                        className="p-1 px-2 ml-2 flex gap-2 transition duration-150 bg-amber-600 hover:bg-amber-700 rounded font-bold">
-                                    <img src={NexusMods} alt="NexusMods Logo" className="h-6 w-6"/>
-                                    Nexus Mods
-                                </button>
-                            </div>
-                        ) :
-                        (
-                            <div className="flex flex-col gap-2">
-                                <h2 className="text-xl">NexusMods Settings</h2>
-                                <div className="flex gap-2 items-center justify-between rounded-lg p-2 bg-muted">
-                                    <div className="flex items-center gap-2 text-lg">
-                                        <img src={NexusMods} alt="NexusMods Logo" className="h-8 w-8"/>
-                                        <p>Logged in as: <span className="font-bold">{user.name}</span></p>
-                                    </div>
-                                    <button onClick={logoutUser}
-                                            className="p-1 px-5 flex gap-2 transition duration-150 bg-destructive hover:brightness-75 rounded">
-                                        Logout
-                                    </button>
-                                </div>
-                                <div className="flex gap-2 items-center mt-4">
-                                    <Switch checked={nmxSwitch} onCheckedChange={x => setNmxSwitch(!nmxSwitch)}/>
-                                    Let Junimo handle the
-                                    <div className="bg-[#d98f40] p-1 px-2 w-fit flex gap-1">
-                                        <img src={NexusMods} alt="Nexus Mods Mod Manager Icon" className="h-full w-6"/>
-                                        MOD MANAGER DOWNLOAD
-                                    </div>
-                                    Button
-                                </div>
-                            </div>
-                        )}
+
+                <div className="flex flex-col">
+                    <Label htmlFor="path" className="mb-1 text-xl">{t("language")}</Label>
+                    <div className="flex gap-2">
+                        <Select value={lang} onValueChange={e => setLang(e)}>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="English"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="en">English</SelectItem>
+                                <SelectItem value="de">German</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div>
+                <div className="w-full h-[2px] border-lg bg-muted"/>
+
+                <div className="flex flex-col">
+                    <Label htmlFor="path" className="mb-1 text-xl">{t("keepOpen")}</Label>
+                    <p className="text-sm text-muted-foreground mb-2">{t("keepOpenDesc")}</p>
+                    <div className="flex gap-2">
+                        <Checkbox checked={keepOpen} onCheckedChange={e => setKeepOpen(!keepOpen)}/>
+                    </div>
                 </div>
             </div>
         </>
