5e312271b0b5f4ee9803468de751a98d0a849e15
Tentastisch <studiotent22@gmail.com>
2024-05-22 13:59:22 +0200

    refactor: Changed the import to the new system

--- src-tauri/src/app/import.rs
+++ src-tauri/src/app/import.rs
@@ -4,12 +4,15 @@ use crate::app::{console, mods, profiles};
 use rfd::FileDialog;
 use std::fs::File;
 use std::path::{Path, PathBuf};
-use std::{fs, io};
+use std::{fs, io, thread};
 use tauri::{command, Manager, WebviewUrl, Window};
 use walkdir::WalkDir;
 use zip::write::SimpleFileOptions;
 use zip::ZipWriter;
 
+/// Opens the import window
+///
+/// * `handle` - The app handle of the Tauri application
 #[command]
 pub async fn open_import(handle: tauri::AppHandle) {
     tauri::WebviewWindowBuilder::new(&handle, "Importer", WebviewUrl::App("/importer".into()))
@@ -21,11 +24,9 @@ pub async fn open_import(handle: tauri::AppHandle) {
         .unwrap();
 }
 
-#[command]
-pub async fn close_import(window: Window) {
-    window.close().unwrap();
-}
-
+/// Opens a file dialog to select a directory to import
+///
+/// # Returns the path of the selected directory, else an empty string
 #[command]
 pub fn select_import_dir() -> String {
     let file = FileDialog::new()
@@ -40,112 +41,53 @@ pub fn select_import_dir() -> String {
     }
 }
 
+/// Imports a profile from a zip file
+///
+/// * `window` - The window to close after importing
+/// * `handle` - The app handle of the Tauri application
+/// * `path` - The path of the zip file to import
+/// * `all` - Whether the import is an "all profiles" import
 #[command]
-pub fn import_profile(handle: tauri::AppHandle, path: &str, all: bool) -> bool {
-    let path_as_buff = Path::new(path);
-    let file = fs::File::open(&path_as_buff).unwrap();
-    let mut file_name = path_as_buff
-        .file_name()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .to_string();
-    file_name = file_name.replace(".zip", "");
+pub fn import_profile(window: Window, handle: tauri::AppHandle, path: &str, all: bool) {
+    let path = path.clone().to_string();
+    let all = all.clone();
+    let window_clone = window.clone();
 
-    let mut temp_path = paths::temp_path();
-    temp_path.push(file_name);
-    fs::create_dir_all(&temp_path).unwrap();
-
-    let mut zip_archive = zip::ZipArchive::new(file).unwrap();
-    zips::extract_zip(zip_archive, &temp_path).unwrap();
-
-    if all {
-        import_all(&handle, &temp_path);
-    } else {
-        import_one(&handle, &temp_path);
-    }
-    &handle.emit("reload", true).unwrap();
-    true
-}
-
-fn import_all(handle: &tauri::AppHandle, temp_path: &PathBuf) {
-    let walkdir = WalkDir::new(&temp_path);
-    let it = walkdir.into_iter();
-
-    for entry in it.filter_map(|e| e.ok()) {
-        let mut junimo_path = paths::appdata_path();
-        let mut junimo_mod_path = paths::mod_path();
-
-        let path = entry.path();
-
-        if path.file_name().unwrap().to_string_lossy().contains(".zip") {
-            junimo_mod_path.push(path.file_name().unwrap().to_string_lossy().to_string());
-            fs::copy(&path, &junimo_mod_path).unwrap();
-        } else if path
+    // Spawn a new thread to import the profile in the background
+    thread::spawn(move || {
+        let path_as_buff = Path::new(&path);
+        let file = fs::File::open(&path_as_buff).unwrap();
+        let mut file_name = path_as_buff
             .file_name()
             .unwrap()
-            .to_string_lossy()
-            .contains(".json")
-        {
-            junimo_path.push(path.file_name().unwrap().to_string_lossy().to_string());
-            fs::copy(&path, &junimo_path).unwrap();
-        }
-    }
-    fs::remove_dir_all(&temp_path).unwrap();
-    let profiles = profiles::get_profiles(paths::profile_path());
-    &handle.emit("profile-update", &profiles).unwrap();
-    console::add_line(
-        &handle,
-        "<span class=\"console-green\">[Junimo] Imported all profiles</span>".to_string(),
-    );
-}
-
-fn import_one(handle: &tauri::AppHandle, temp_path: &PathBuf) {
-    let mut mods = mods::get_all_mods();
-    let mut profiles = profiles::get_profiles(paths::profile_path());
-
-    let walkdir = WalkDir::new(&temp_path);
-    let it = walkdir.into_iter();
-
-    for entry in it.filter_map(|e| e.ok()) {
-        let mut junimo_path = paths::appdata_path();
-        let mut junimo_mod_path = paths::mod_path();
-
-        let path = entry.path();
-
-        if path.file_name().unwrap().to_string_lossy().contains(".zip") {
-            junimo_mod_path.push(path.file_name().unwrap().to_string_lossy().to_string());
-            let mut check_path = junimo_mod_path.clone();
-            check_path.push(path.file_name().unwrap().to_string_lossy().to_string());
-            if !check_path.exists() {
-                fs::copy(&path, &junimo_mod_path).unwrap();
-            }
-        } else if path
-            .file_name()
+            .to_str()
             .unwrap()
-            .to_string_lossy()
-            .contains("profile.json")
-        {
-            let data_raw = fs::read_to_string(path).unwrap();
-            let data = data_raw.as_str();
-            let import_profile: Vec<Profile> = serde_json::from_str(data).unwrap();
-            let mut cloned_profile = import_profile[0].clone();
-            cloned_profile.currently = false;
-            profiles.insert(1, cloned_profile);
+            .to_string();
+        file_name = file_name.replace(".zip", "");
 
-            for mod_info in &import_profile[0].mods {
-                if (mods.iter().find(|&x| x.name == mod_info.name)).is_none() {
-                    mods.push(mod_info.clone());
-                }
+        let zip_archive = zip::ZipArchive::new(file).unwrap();
+        let import_result = zips::import_zip(zip_archive, &paths::appdata_path(), &paths::temp_path(), all);
+        match import_result {
+            Ok(_) => {
+                console::add_line(
+                    &handle,
+                    format!(
+                        "<span class=\"console-green\">[Junimo] Imported profile(s) {}</span>",
+                        file_name
+                    ),
+                );
+            }
+            Err(e) => {
+                console::add_line(
+                    &handle,
+                    format!(
+                        "<span class=\"console-green\">[Junimo] Failed to import profile {}: {}</span>",
+                        file_name,
+                        e
+                    ),
+                );
             }
         }
-    }
-    profiles::save_profiles(&profiles, &paths::profile_path());
-    mods::save_mods(mods);
-    fs::remove_dir_all(&temp_path).unwrap();
-    &handle.emit("profile-update", &profiles).unwrap();
-    console::add_line(
-        &handle,
-        "<span class=\"console-green\">[Junimo] Imported profile</span>".to_string(),
-    );
+        window_clone.close().unwrap();
+    });
 }
